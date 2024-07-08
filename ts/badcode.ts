918c7a203f76f2864dd7eb560976de6bdcea5d4f
Edar Chan <115542005+edar-chan-mhra@users.noreply.github.com>
2024-07-08 18:51:40 +0100

    Update badcode.ts
    even less secure code

--- ts/badcode.ts
+++ ts/badcode.ts
@@ -1,54 +1,65 @@
-// bad_code.ts
+// bad_code.js
 
-import * as http from 'http';
-import * as url from 'url';
-import * as fs from 'fs';
-import * as mysql from 'mysql';
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var mysql = require('mysql');
+var crypto = require('crypto');
 
-const connection = mysql.createConnection({
+// Connection details should not be hardcoded
+var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'testdb'
 });
 
-connection.connect((err: mysql.MysqlError | null) => {
+connection.connect(function(err) {
     if (err) throw err;
     console.log('Connected to database');
 });
 
-http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    const q = url.parse(req.url || '', true);
-    const pathname = q.pathname;
-    const query = q.query;
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var pathname = q.pathname;
+    var query = q.query;
 
     if (pathname === "/") {
-        fs.readFile('index.html', (err, data) => {
+        fs.readFile('index.html', function(err, data) {
             if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.writeHead(404, {'Content-Type': 'text/html'});
                 res.write("404 Not Found");
                 return res.end();
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+            } 
+            res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
             return res.end();
         });
     } else if (pathname === "/search") {
-        const searchQuery = query.q as string;
-        connection.query(`SELECT * FROM users WHERE name = '${searchQuery}'`, (err, result) => {
+        var searchQuery = query.q;
+        // Vulnerable to SQL Injection
+        connection.query("SELECT * FROM users WHERE name = '" + searchQuery + "'", function (err, result) {
             if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.writeHead(200, {'Content-Type': 'application/json'});
             res.write(JSON.stringify(result));
             return res.end();
         });
     } else if (pathname === "/eval") {
-        const code = query.code as string;
-        eval(code); // Dangerous: using eval with user input
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        var code = query.code;
+        // Extremely dangerous: using eval with user input
+        eval(code);
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write("Code executed");
         return res.end();
+    } else if (pathname === "/md5") {
+        var input = query.input;
+        // Use of weak hash function (MD5)
+        var hash = crypto.createHash('md5').update(input).digest('hex');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write("MD5 Hash: " + hash);
+        return res.end();
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.writeHead(404, {'Content-Type': 'text/html'});
         res.write("404 Not Found");
         return res.end();
     }
@@ -57,9 +68,11 @@ http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
 console.log('Server running at http://localhost:8080/');
 
 // Issues:
-// - SQL Injection: Directly inserting user input into SQL query without sanitization
+// - SQL Injection: User input is directly inserted into SQL query without sanitization
 // - XSS: Not escaping user input before displaying it on the page
 // - Use of eval(): Executing user-provided code
+// - Hardcoded database credentials
+// - Use of weak hash function (MD5)
 // - Lack of proper error handling and logging
 // - Inconsistent use of single and double quotes
 // - No input validation or sanitization
