2845b6aeecaf6f01508af14430d86247f00f3bb0
Edar Chan <115542005+edar-chan-mhra@users.noreply.github.com>
2024-07-08 18:56:54 +0100

    Update badcode.ts

--- ts/badcode.ts
+++ ts/badcode.ts
@@ -1,65 +1,65 @@
-// bad_code.js
+// bad_code.ts
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var mysql = require('mysql');
-var crypto = require('crypto');
+import * as http from 'http';
+import * as url from 'url';
+import * as fs from 'fs';
+import * as mysql from 'mysql';
+import * as crypto from 'crypto';
 
 // Connection details should not be hardcoded
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'testdb'
 });
 
-connection.connect(function(err) {
+connection.connect((err: mysql.MysqlError | null) => {
     if (err) throw err;
     console.log('Connected to database');
 });
 
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var pathname = q.pathname;
-    var query = q.query;
+http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    const q = url.parse(req.url || '', true);
+    const pathname = q.pathname;
+    const query = q.query;
 
     if (pathname === "/") {
-        fs.readFile('index.html', function(err, data) {
+        fs.readFile('index.html', (err, data) => {
             if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.writeHead(404, { 'Content-Type': 'text/html' });
                 res.write("404 Not Found");
                 return res.end();
-            } 
-            res.writeHead(200, {'Content-Type': 'text/html'});
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             return res.end();
         });
     } else if (pathname === "/search") {
-        var searchQuery = query.q;
+        const searchQuery = query.q as string;
         // Vulnerable to SQL Injection
-        connection.query("SELECT * FROM users WHERE name = '" + searchQuery + "'", function (err, result) {
+        connection.query(`SELECT * FROM users WHERE name = '${searchQuery}'`, (err, result) => {
             if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.writeHead(200, { 'Content-Type': 'application/json' });
             res.write(JSON.stringify(result));
             return res.end();
         });
     } else if (pathname === "/eval") {
-        var code = query.code;
+        const code = query.code as string;
         // Extremely dangerous: using eval with user input
         eval(code);
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write("Code executed");
         return res.end();
     } else if (pathname === "/md5") {
-        var input = query.input;
+        const input = query.input as string;
         // Use of weak hash function (MD5)
-        var hash = crypto.createHash('md5').update(input).digest('hex');
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        const hash = crypto.createHash('md5').update(input).digest('hex');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write("MD5 Hash: " + hash);
         return res.end();
     } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.writeHead(404, { 'Content-Type': 'text/html' });
         res.write("404 Not Found");
         return res.end();
     }
